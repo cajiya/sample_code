18b90e62f12dc026539a41422ca582e37c1ea075
nakamura keisuke <k_nakamura@m.cajiya.co.jp>
2022-08-07 20:11:25 +0900

    Create 0807_WP_eccube_wp_head.php

---
+++ 2022/0807_WP_eccube_wp_head.php
@@ -0,0 +1,20 @@
+if( !function_exists('eccube_wp_head') ) :
+function eccube_wp_head( $content ){
+?>
+<script>
+window.addEventListener('DOMContentLoaded',()=>{
+    var CartElm = document.querySelector('.js-ec-cart');
+    if( CartElm ){
+        var CartRequest = new XMLHttpRequest();
+        CartRequest.open("GET", "/ec-cube-path"); // EC-CUBEのインストールPATHに変更してください
+        CartRequest.addEventListener("load", function(){
+            CartElm.innerHTML = this.responseText;
+        });
+        CartRequest.send();
+    }
+})
+</script>
+<?php
+}
+endif;
+add_action( 'wp_head', 'eccube_wp_head' , 10 , 1 );
